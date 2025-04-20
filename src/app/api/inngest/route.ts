a706dc955670209a344758065538ae1c06a9b19f
Krzysztof Ciach <kspacja@gmail.com>
2025-04-20 14:42:34 +0200

    test job

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,9 @@
 import { inngest } from '@/inngest';
+import { helloWorld } from '@/inngest/function';
 import { serve } from 'inngest/next';
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-  ],
+  functions: [helloWorld],
 });
